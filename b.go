870da61ab588f50a95868e80e35e21ffcef6eb74
lichunpeng <licp@knowbox.cn>
2021-04-22 17:33:20 +0800

    .

--- b.go
+++ b.go
@@ -2,7 +2,7 @@ package modb
 
 import "fmt"
 
-var Version="v3.0.9"
+var Version="v4.0.0"
 
 func Print() {
 	fmt.Println("modb", Version)
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/lcp8474140/modb/v3
+module github.com/lcp8474140/modb/v4
 
 go 1.13
